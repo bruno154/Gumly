b080fb7d113270255fb10f01b87d88ae5a8adbe4
Bruno VINICIUS NONATO <bruno_vinicius_nonato@internal.carrefour.com>
2022-06-28 17:40:13 -0300

    gcp_connectors

--- gumly/gcp-connectors.py
+++ gumly/gcp-connectors.py
@@ -1,7 +1,9 @@
 import logging
+import pandas as pd
 from google.cloud import storage
 from google.cloud import bigquery
-from typing import List, NamedTuple, Union, Dict, Tuple
+from pandas.io.json import build_table_schema
+
 
 
 
@@ -14,7 +16,7 @@ def create_bucket(bucket_name:str, project_id:str):
 	Return: None
     """
     
-    storage_client = storage.Client()
+    storage_client = storage.Client(project=project_id)
     bucket = storage_client.bucket(bucket_name)
     bucket.storage_class = "COLDLINE"
     new_bucket = storage_client.create_bucket(bucket, project=project_id, location="us")
@@ -52,34 +54,44 @@ def create_bq_dataset(dataset_name:str, project_id:str):
 
 	return dataset
 
-def get_table_schema(table_name, schema):
+def get_schema_bqtable(table_name, project_id):
 
-    """
+    """A function to get the schema form a table in Bigquery
+
+    Params:
+        table_name: The table name were we will get the schema from.
+        project_id: The id of the project were the table is. 
+
+    Return: 
+        List with table_schema information
     
     """
     
-    client_bq = bigquery.Client(project=schema)
+    client_bq = bigquery.Client(project=project_id)
     table = client_bq.get_table(table_name)
     table_schema = [{"name": schema.name, "type": schema.field_type} for schema in table.schema]
     return table_schema
 
-def retrieve_data_bq(query: str,
-                     project_id: str,
+def bq_select_create(project_id: str,
+                     query: str = None,
                      df_to_bq: pd.DataFrame = None,
+                     dataset: str = None,
                      destination_table: str = None,
-                     schema: dict = None,
                      sql_type: str = "SELECT",
                      verbose: bool = False
-                     ) ->NamedTuple("dataset", [("data", pd.DataFrame)]): 
-    """
+                     )-> pd.DataFrame: 
+    
+    """ A function to retrieve data from bigquery as a pandas dataframe and 
+        to create a table from a pandas dataframe on bigquery. 
+
     Params:
-        query:
-        project_id:
-        df_to_bq:
-        destination_table:
-        schema:
-        sql_type:
-        verbose:
+        query:             The sql query
+        project_id:        The GCP project id were the bucket will be created. 
+        df_to_bq:          The dataframe from were the bq table will be created.
+        dataset:           The name of the dataset in bigquery.
+        destination_table: The path were the new bq talble will be created. In the form projectId.datasetId.tableId
+        sql_type:          If the operation is going to be `SELECT` to retrieve data or `CREATE` to create a new table.
+        verbose:           Enable logging.
     
     Return:
         dataframe: A Dataframe object with the result of the query
@@ -90,7 +102,7 @@ def retrieve_data_bq(query: str,
     if  isinstance(sql_type, str):
             sql_type = sql_type
     else:
-        raise Exception(f"Argument sql_type must be a string")
+        raise Exception("Argument sql_type must be a string")
 
     # Client
     bq_client = bigquery.Client(project=project_id)
@@ -105,12 +117,25 @@ def retrieve_data_bq(query: str,
             .result()
             .to_dataframe(create_bqstorage_client=True)
         )
+        
+        return data
 
-        dataframe = namedtuple("dataset", ["data"])
-        return dataframe(data=data)
-    else:
+    elif sql_type == 'CREATE':
+
+        destination_table = f'{project_id}.{dataset}.{destination_table}'
+        df_to_bq.to_gbq(destination_table=destination_table, project_id=project_id, if_exists="fail")
+        
         if verbose:
+
             logging.debug(query)
-        df_to_bq.to_gbq(destination_table=destination_table, project_id=project_id, if_exists="fail", table_schema=schema)
-        if verbose:
-            return print(f"Created table {destination_table} at project {project_id} and schema {schema}")
+            print(f"Created table {destination_table} at project {project_id}")
+    else:
+
+        raise Exception(f"Forbidden operation the parameter sql_type shoulbe either SELECT or CREATE.")
+
+
+
+
+
+
+
--- gumly/kubeflow-components.py
+++ gumly/kubeflow-components.py
@@ -1,4 +1,6 @@
 from kfp.v2.dsl import component
+from collections import namedtuple
+from typing import NamedTuple #Union, Dict, Tuple, List
 
 @component(
     base_image="python:3.9",
@@ -27,23 +29,3 @@ def create_bucket(bucket_name:str, project_id:str):
         com storage class {new_bucket.storage_class}""")
     
     return new_bucket
-
-@component(
-    base_image="python:3.9",
-    packages_to_install=["google-cloud-pipeline-components", "googleapis-common-protos"]
-)
-def tabular_data(project_id: str,
-                 display_name: str,
-                 bq_source: str,
-                 label: str
-                ):
-
-    from google_cloud_pipeline_components import aiplatform as gcc_aip
-
-    dataset = gcc_aip.TabularDatasetCreateOp(
-        project = project_id,
-        display_name = display_name,
-        bq_source = bq_source,
-        labels = {'notebook':f'{label}'}
-    )
-    return dataset
